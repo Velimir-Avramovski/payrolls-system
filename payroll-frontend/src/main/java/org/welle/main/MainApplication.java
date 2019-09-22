9ece4df6096c6ffde3c511ec98ff01a89d09dc11
Velimir Avramovski <velimir.avrv@gmail.com>
2019-09-22 10:03:30 +0200

    Refactor.

---
+++ payroll-frontend/src/main/java/org/welle/main/MainApplication.java
@@ -0,0 +1,12 @@
+package org.welle.main;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+// Wasting days on this bullshit!
+@ApplicationPath("/payrolls-client")
+public class MainApplication extends Application {
+
+    // Empty for now.
+
+}
